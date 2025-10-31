deb40e6e3229302007da569545485f46bf180f07
Tom Mwambingu <[tomchom30@gmail.com]>
2025-10-31 08:42:32 +0300

    implemented the command to try every password till get the one

--- password.py
+++ password.py
@@ -4,3 +4,6 @@ password = input("Enter your password: ")
 start = time.time()
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:,.<>?/`~ '
 guess = []
+for val in range(5):
+    a = [i for i in chars]
+    for y in range(val):
